3adb54e0df844e4c809804dae86468b4f95dd9f6
thomas <tdougherty16@gmail.com>
2023-08-22 23:13:02 -0400

    led switch

--- switch/switch.ino
+++ switch/switch.ino
@@ -1,18 +1,23 @@
 int switch_pin = 2;     //connect switch to digitial pin #2
+int led_pin = 12;
 int volt;
 void setup() {
-    Serial.begin(9600);
+    //Serial.begin(9600);
+    pinMode(led_pin, OUTPUT);
     pinMode(switch_pin, INPUT);
-    Serial.println("start");
 }
 
 
 void loop(){
     volt = digitalRead(switch_pin);
-    if(volt == HIGH){
-        Serial.println("Button pressed");
-    } else {
-        Serial.println("Button released");
+    if(volt == LOW) {
+        digitalWrite(led_pin, HIGH);
+        delay(100);
+        digitalWrite(led_pin, LOW);
+        delay(100);
+    } else if(volt == HIGH) {
+     //   Serial.println("Button pressed");
+        digitalWrite(led_pin, LOW);
     }
-    delay(100);
 }
+
