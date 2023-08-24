3378cf802d55c73175c261f11ba5c03fe4ced913
thomas <tdougherty16@gmail.com>
2023-08-23 21:07:43 -0400

    alternating switch

--- switch/switch.ino
+++ switch/switch.ino
@@ -1,32 +1,26 @@
 int switch_pin = 2;     //connect switch to digitial pin #2
-int led_pin12 = 12;
-int led_pin11 = 11;
-int volt;
-int count;
+int val;
+int button_state;
 
-void flash (int pin);
+
 
 void setup() {
     Serial.begin(9600);
-    pinMode(led_pin12, OUTPUT);
-    pinMode(led_pin11, OUTPUT);
     pinMode(switch_pin, INPUT);
+
+    button_state = digitalRead(switch_pin);     // init button state as input value
 }
 
 
 void loop(){
-    volt = digitalRead(switch_pin);
-    if(volt == LOW) {
-        flash(led_pin12);
-    } else if(volt == HIGH) {
-        digitalWrite(led_pin11, HIGH);
-        digitalWrite(led_pin12, LOW);
-    }
-}
+    val = digitalRead(switch_pin);              // read input value
 
-void flash(int pin){
-    digitalWrite(pin, HIGH);
-    delay(100);
-    digitalWrite(pin, LOW);
-    delay(100);
+    if(val != button_state) {                   // check if button state changed
+        if(val == LOW) {                        // check if button is pressed
+            Serial.println("Button pressed");
+        } else if(val == HIGH) {
+            Serial.println("Button released");
+        }
+    }
+    button_state = val; // temporarily store button state
 }
